c83a601adff896ad2c273490f4d489d52e740d97
Medhat Mostafa <mmedhatmostafa3@gmail.com>
2021-02-27 06:54:24 +0200

    master

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-// import 'package:ecommerce/backend/providers/cart_provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
